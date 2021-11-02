a1da94ae40de4a80cd22e745e9ff18fa5245ef0b
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    sdm660-common: Makefile: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Init
 PRODUCT_PACKAGES += \
     init.class_main.sh \
