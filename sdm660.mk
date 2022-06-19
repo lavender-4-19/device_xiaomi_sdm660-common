af6847bce197063c6213ba620f9757f0465b2f71
Vitorgl2003 <vitorgl.2003@gmail.com>
2022-06-19 16:40:41 +0100

    sdm660-common: Drop useless camera packages - We aren't on arrow.
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -123,14 +123,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
-ifeq ($(ARROW_GAPPS), true)
-PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
-else
-PRODUCT_PACKAGES += \
-    Snap
-endif
-
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.5:64 \
